835d531cc5e19ebb7da720efa4ebd1d2ee3a6dd3
ezechielSeboua <ezechiel.seboua@epitech.eu>
2026-06-17 14:13:45 +0000

    Fix: Suppression de l'import ../../components/info dans NewsPage/

--- src/pages/News/NewsPage.jsx
+++ src/pages/News/NewsPage.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
-import Info from '../../components/info'; // Intégration du composant d'information
 
 const NewsPage = () => {
   // Gestion de l'onglet actif : ALL (Tout), NEWS (Actualités), PRESS (Press Book)
